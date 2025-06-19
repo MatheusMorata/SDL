277f69e9ac0630afdce2cd5828df020e44394393
Matheus <65503571+MatheusMorata@users.noreply.github.com>
2025-06-19 16:15:03 -0300

    FEAT: Adicionando eventos via teclado

--- Exemplo1.c
+++ Exemplo1.c
@@ -1,36 +1,36 @@
 #include <SDL.h>
+#include <stdio.h>
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[]) {
 
     SDL_Window *janela = NULL;
     SDL_Renderer *render = NULL;
 
     // Verifica erro na inicialização do SDL
-    if(SDL_Init(SDL_INIT_VIDEO) < 0){
-        printf("Erro ao iniciar SDL: %s", SDL_GetError());
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("Erro ao iniciar SDL: %s\n", SDL_GetError());
         return 1;
     }
 
     // Cria janela
     janela = SDL_CreateWindow(
         "Ola Mundo", // Título da janela
-        SDL_WINDOWPOS_CENTERED, // Posição X da janela
-        SDL_WINDOWPOS_CENTERED, // Posição Y da janela
-        800,600, // Tamanho da janela
-        SDL_WINDOW_SHOWN // Indica se a janela está visível
+        SDL_WINDOWPOS_CENTERED, // Posição X
+        SDL_WINDOWPOS_CENTERED, // Posição Y
+        800, 600, // Tamanho da janela
+        SDL_WINDOW_SHOWN // Exibir a janela
     );
 
     // Verifica erro na criação da janela
-    if(!janela){
-        printf("Erro ao criar janela: %s", SDL_GetError());
+    if (!janela) {
+        printf("Erro ao criar janela: %s\n", SDL_GetError());
+        SDL_Quit();
         return 1;
     }
 
     // Cria o render
     render = SDL_CreateRenderer(janela, -1, SDL_RENDERER_ACCELERATED);
-
-    // Verifica erro na criação do render
-    if(!render){
+    if (!render) {
         printf("Erro ao criar render: %s\n", SDL_GetError());
         SDL_DestroyWindow(janela);
         SDL_Quit();
@@ -44,13 +44,34 @@ int main(int argc, char *argv[]){
     // Apresenta na tela
     SDL_RenderPresent(render);
 
-    // Loop de eventos - Espera até o usuário fechar
+    // Loop de eventos
     SDL_Event event;
     int running = 1;
+
     while (running) {
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_QUIT) {
                 running = 0;
+            }
+
+            // Evento de teclado
+            if (event.type == SDL_KEYDOWN) {
+                switch (event.key.keysym.sym) {
+                    case SDLK_w:
+                        printf("Tecla W pressionada\n");
+                        break;
+                    case SDLK_a:
+                        printf("Tecla A pressionada\n");
+                        break;
+                    case SDLK_s:
+                        printf("Tecla S pressionada\n");
+                        break;
+                    case SDLK_d:
+                        printf("Tecla D pressionada\n");
+                        break;
+                    default:
+                        break;
+                }
             }
         }
     }
