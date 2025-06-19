7f938e996ce36dfbf024dd5c3ca9249b18792d3f
Matheus <65503571+MatheusMorata@users.noreply.github.com>
2025-06-19 11:24:05 -0300

    FEAT: Criando Janela Básica

---
+++ Exemplo1.c
@@ -0,0 +1,64 @@
+#include <SDL.h>
+
+int main(int argc, char *argv[]){
+
+    SDL_Window *janela = NULL;
+    SDL_Renderer *render = NULL;
+
+    // Verifica erro na inicialização do SDL
+    if(SDL_Init(SDL_INIT_VIDEO) < 0){
+        printf("Erro ao iniciar SDL: %s", SDL_GetError());
+        return 1;
+    }
+
+    // Cria janela
+    janela = SDL_CreateWindow(
+        "Ola Mundo",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        800,600,
+        SDL_WINDOW_SHOWN
+    );
+
+    // Verifica erro na criação da janela
+    if(!janela){
+        printf("Erro ao criar janela: %s", SDL_GetError());
+        return 1;
+    }
+
+    // Cria o render
+    render = SDL_CreateRenderer(janela, -1, SDL_RENDERER_ACCELERATED);
+
+    // Verifica erro na criação do render
+    if(!render){
+        printf("Erro ao criar render: %s\n", SDL_GetError());
+        SDL_DestroyWindow(janela);
+        SDL_Quit();
+        return 1;
+    }
+
+    // Cor de fundo: branco
+    SDL_SetRenderDrawColor(render, 255, 255, 255, 255);
+    SDL_RenderClear(render);
+
+    // Apresenta na tela
+    SDL_RenderPresent(render);
+
+    // Loop de eventos - Espera até o usuário fechar
+    SDL_Event event;
+    int running = 1;
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = 0;
+            }
+        }
+    }
+
+    // Limpeza
+    SDL_DestroyRenderer(render);
+    SDL_DestroyWindow(janela);
+    SDL_Quit();
+
+    return 0;
+}
