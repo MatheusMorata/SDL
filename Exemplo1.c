70bb77f687c6a0d81274680fbd04a1ffd2742466
Maicon Renildo <maicon.renildo@maravi.com.br>
2025-07-13 16:22:32 -0300

    refactor: ajusta exemplo com base no uso do utils

--- Exemplo1.c
+++ Exemplo1.c
@@ -1,6 +1,8 @@
 #include <SDL.h>
 #include <stdio.h>
 #include <time.h>
+#include <stdbool.h>
+#include "sdl_utils.h"
 
 // Função para verificar colisão entre dois retângulos (quadrados)
 int colisao_rects(SDL_Rect a, SDL_Rect b) {
@@ -20,41 +22,13 @@ void colisao(SDL_Rect quadrado, SDL_Rect quadrados[], int consumidos[]) {
 }
 
 int main(int argc, char* argv[]) {
-    SDL_Window* janela = NULL;
-    SDL_Renderer* render = NULL;
-    srand(time(NULL)); // Inicializa números aleatórios 
-
-    // Inicializa o SDL
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        printf("Erro ao iniciar SDL: %s\n", SDL_GetError());
+    SDL_Resources sdl_resources = open_sdl("Quadradinhos");
+    if (!sdl_resources.success) {
+        printf("%s\n", sdl_resources.error);
         return 1;
     }
 
-    // Cria a janela
-    janela = SDL_CreateWindow(
-        "Ola Mundo",
-        SDL_WINDOWPOS_CENTERED,
-        SDL_WINDOWPOS_CENTERED,
-        800, 600,
-        SDL_WINDOW_SHOWN
-    );
-
-    // Verifica erros na criação da janela
-    if (!janela) {
-        printf("Erro ao criar janela: %s\n", SDL_GetError());
-        SDL_Quit();
-        return 1;
-    }
-
-    // Cria o renderizador
-    render = SDL_CreateRenderer(janela, -1, SDL_RENDERER_ACCELERATED);
-    if (!render) {
-        printf("Erro ao criar render: %s\n", SDL_GetError());
-        SDL_DestroyWindow(janela);
-        SDL_Quit();
-        return 1;
-    }
-
+    srand(time(NULL));
     // Posição inicial do quadrado amarelo
     int x = 400;
     int y = 300;
@@ -62,7 +36,7 @@ int main(int argc, char* argv[]) {
     SDL_Rect quadrado = { x, y, tamanho, tamanho };
 
     // Inicializa os quadrados vermelhos e vetor de consumidos
-    int consumidos[10] = {0};  
+    int consumidos[10] = {0};
     SDL_Rect quadrados[10];
     for (int i = 0; i < 10; i++) {
         quadrados[i].x = rand() % (800 - tamanho);
@@ -82,7 +56,7 @@ int main(int argc, char* argv[]) {
         time_t agora = time(NULL);
         if (difftime(agora, tempo_inicio) >= 30) {
             printf("VOCÊ PERDEU!\n");
-            break;  
+            break;
         }
 
         // Verifica se o jogador venceu (todos os quadrados foram consumidos)
@@ -95,7 +69,7 @@ int main(int argc, char* argv[]) {
         }
         if (venceu) {
             printf("VOCÊ GANHOU!\n");
-            break;  
+            break;
         }
 
         // Processa os eventos (teclado e fechamento de janela)
@@ -129,31 +103,28 @@ int main(int argc, char* argv[]) {
         colisao(quadrado, quadrados, consumidos);
 
         // Limpa a tela (define cor de fundo branca)
-        SDL_SetRenderDrawColor(render, 255, 255, 255, 255);
-        SDL_RenderClear(render);
+        SDL_SetRenderDrawColor(sdl_resources.render, 255, 255, 255, 255);
+        SDL_RenderClear(sdl_resources.render);
 
         // Desenha os quadrados vermelhos restantes
         for (int i = 0; i < 10; i++) {
             if (!consumidos[i]) {
-                SDL_SetRenderDrawColor(render, 255, 0, 0, 255);
-                SDL_RenderFillRect(render, &quadrados[i]);
+                SDL_SetRenderDrawColor(sdl_resources.render, 255, 0, 0, 255);
+                SDL_RenderFillRect(sdl_resources.render, &quadrados[i]);
             }
         }
 
         // Desenha o quadrado amarelo (jogador)
-        SDL_SetRenderDrawColor(render, 255, 255, 0, 255);
-        SDL_RenderFillRect(render, &quadrado);
+        SDL_SetRenderDrawColor(sdl_resources.render, 255, 255, 0, 255);
+        SDL_RenderFillRect(sdl_resources.render, &quadrado);
 
         // Atualiza a tela
-        SDL_RenderPresent(render);
+        SDL_RenderPresent(sdl_resources.render);
 
         SDL_Delay(16);  // 60 FPS
     }
 
-    // Limpeza 
-    SDL_DestroyRenderer(render);
-    SDL_DestroyWindow(janela);
-    SDL_Quit();
+    close_sdl(sdl_resources);
 
     return 0;
 }
