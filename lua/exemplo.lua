37268c7a7bb7731a7fbed923d3ab733ac2abe2a8
MatheusMorata <matlucar09@hotmail.com>
2025-08-01 13:09:36 -0300

    feat: Criando janela

--- lua/exemplo.lua
+++ lua/exemplo.lua
@@ -1,1 +1,11 @@
 local SDL = require("SDL") -- Importando a biblioteca lua-sdl2
+
+-- Criando a janela
+local janela = assert(SDL.createWindow{
+    titulo = 'Exemplo',
+    largura = 600,
+    altura = 800,
+    flags = {SDL.Shown},
+    inicial_x = 400,
+    inicial_y = 400,
+})
