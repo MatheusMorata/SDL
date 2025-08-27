21c157f3e68cc2983e2a2c3f2f778c897902fc5b
MatheusMorata <eumatheus09@gmail.com>
2025-08-27 17:02:43 -0300

    feat: Adicionado renderizador e coloração para o fundo

--- c/overleaf.c
+++ c/overleaf.c
@@ -14,6 +14,13 @@ int main(int argc, char* args[]){
         SDL_WINDOW_SHOWN // Mantem a janela visivel
     );
 
+    // Criando renderizador
+    SDL_Renderer *renderizador = SDL_CreateRenderer(janela, -1, 0);
+
+    SDL_SetRenderDrawColor(renderizador, 255, 255, 255, 0); // Colorindo fundo (branco)
+    SDL_RenderClear(renderizador); // Limpa o renderizador
+    SDL_RenderPresent(renderizador); // Atualizando a tela
+
     // Janela fica aberta por 3 segundos
     SDL_Delay(3000);
 
