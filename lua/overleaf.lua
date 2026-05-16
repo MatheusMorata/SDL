ee1751a25e8e933ca1641b49f0b2c60cbb4b7fa9
MatheusMorata <matlucar09@hotmail.com>
2026-05-16 16:28:52 -0300

    fix bug

--- lua/overleaf.lua
+++ lua/overleaf.lua
@@ -1,27 +1,25 @@
-local SDL = require("SDL")
+local SDL   = require "SDL"
 
-local LARGURA = 800
-local ALTURA = 600
+local LARGURA, ALTURA = 800, 600
 
-SDL.init(SDL.flags.Everything)
-
+SDL.init{SDL.flags.Everything}
 local janela = SDL.createWindow{
-    title = 'Exemplo', 
-    width = LARGURA, 
-    height = ALTURA, 
-    flags = {SDL.window.Resizable}, 
-    x = SDL.window.centralized, 
-    y = SDL.window.centralized, 
+    title = "Hello World", 
+    x = SDL.WINDOWPOS_CENTERED,
+    y = SDL.WINDOWPOS_CENTERED,
+    width = LARGURA,
+    height = ALTURA,
+    flags = SDL.WINDOW_SHOWN
 }
-
+                
 local renderizador = SDL.createRenderer(janela, -1, 0)
 
-renderizador:setDrawColor({0, 0, 0, 255})
+renderizador:setDrawColor({
+    r = 0,
+    g = 0,
+    b = 0
+}) 
+
 renderizador:clear()
 renderizador:present()
-
 SDL.delay(3000)
-
-renderizador:destroy()
-janela:destroy()
-SDL.quit()
