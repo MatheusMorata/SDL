ec4f4eec81fa9c3b362972c463795dfefb2df886
MatheusMorata <matlucar09@hotmail.com>
2026-05-16 16:10:22 -0300

    add: Adicionado vermelho.c

---
+++ c/vermelho.c
@@ -0,0 +1,22 @@
+#include <SDL2/SDL.h>
+
+int main(void) {
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window*   win = SDL_CreateWindow("ex", 0, 0, 640, 360, SDL_WINDOW_SHOWN);
+    SDL_Renderer* ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
+
+    SDL_SetRenderDrawColor(ren, 0, 0, 0, 255);
+    SDL_RenderClear(ren);
+
+    SDL_SetRenderDrawColor(ren, 255, 0, 0, 255);
+    SDL_Rect r = { 10, 10, 20, 20 };
+    SDL_RenderFillRect(ren, &r);
+
+    SDL_RenderPresent(ren);
+
+    SDL_Delay(3000);
+    SDL_DestroyRenderer(ren);
+    SDL_DestroyWindow(win);
+    SDL_Quit();
+    return 0;
+}
