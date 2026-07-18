eb9c4f7146bee2149a09408cf134fee9d707486d
MatheusMorata <matlucar09@hotmail.com>
2026-07-18 10:52:49 -0300

    add: Adicionado pico_structs.c

---
+++ c/pico_structs.c
@@ -0,0 +1,11 @@
+#include "pico.h"
+
+int main (void) {
+    pico_init(1);
+
+    pico_set_color_draw((Pico_Color){255, 0, 0, 255});
+    pico_output_draw_rect((Pico_Rect){10, 10, 20, 15});
+
+    pico_init(0);
+    return 0;
+}
