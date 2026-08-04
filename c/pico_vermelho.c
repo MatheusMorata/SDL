d8962b01537f00f655f178a9bd02824c9c67382d
Matheus <65503571+MatheusMorata@users.noreply.github.com>
2026-08-04 10:22:43 -0300

    feat: Atualizado pico_vermelho.c

--- c/pico_vermelho.c
+++ c/pico_vermelho.c
@@ -2,9 +2,19 @@
 
 int main(void) {
     pico_init(1);
+
+    pico_set_size((Pico_Dim){640, 360}, (Pico_Dim){640, 360});
+
+    pico_set_color_clear((Pico_Color){0, 0, 0, 255});
+    pico_output_clear();
+
     pico_set_color_draw((Pico_Color){255, 0, 0, 255});
     pico_output_draw_rect((Pico_Rect){10, 10, 20, 20});
+
+    pico_output_present();
+
     pico_input_delay(3000);
+
     pico_init(0);
     return 0;
 }
