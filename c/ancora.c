84beb709160917b44ab8caf4c33a836292e96d83
Matheus <65503571+MatheusMorata@users.noreply.github.com>
2026-08-06 13:01:28 -0300

    refactor: Alterações ancora.c

--- c/ancora.c
+++ c/ancora.c
@@ -1,29 +1,42 @@
 #include <SDL2/SDL.h>
 
-int main(void) {
+int main(void)
+{
     SDL_Init(SDL_INIT_VIDEO);
-    SDL_Window*   win = SDL_CreateWindow("ex", 0, 0, 640, 360, SDL_WINDOW_SHOWN);
-    SDL_Renderer* ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
 
-    SDL_SetRenderDrawColor(ren, 0, 0, 0, 255);
-    SDL_RenderClear(ren);
+    SDL_Window *window = SDL_CreateWindow(
+        "SDL2",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        800, 600,
+        SDL_WINDOW_SHOWN
+    );
 
-    int sw = 640, sh = 360; 
-    int rw =  60, rh =  60;  
+    SDL_Renderer *renderer = SDL_CreateRenderer(
+        window, -1,
+        SDL_RENDERER_ACCELERATED
+    );
 
-    SDL_SetRenderDrawColor(ren, 255, 0, 0, 255);
-    SDL_Rect r = {
-        (sw - rw) / 2,   
-        (sh - rh) / 2,   
-        rw, rh
-    };
-    SDL_RenderFillRect(ren, &r);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
 
-    SDL_RenderPresent(ren);
+    SDL_Rect rect;
+    rect.w = 200;
+    rect.h = 100;
+
+    rect.x = 400 - rect.w / 2;
+    rect.y = 300 - rect.h / 2;
+
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    SDL_RenderFillRect(renderer, &rect);
+
+    SDL_RenderPresent(renderer);
+
     SDL_Delay(3000);
 
-    SDL_DestroyRenderer(ren);
-    SDL_DestroyWindow(win);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
     SDL_Quit();
+
     return 0;
 }
