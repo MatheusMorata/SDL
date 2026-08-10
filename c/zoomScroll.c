a30681e9baf93a31dd15428a3a14151f780d39eb
Maicon Renildo <63758491+MaiconRenildo@users.noreply.github.com>
2026-08-10 20:46:47 -0300

    Update pico_pos initialization to use Pico_Pct

--- c/zoomScroll.c
+++ c/zoomScroll.c
@@ -4,7 +4,7 @@ int main () {
      pico_init (1) ;
      pico_set_size (( Pico_Dim ) {450 , 300} , (Pico_Dim ) {150 , 100});
      pico_set_grid (0);
-     Pico_Pos pos = pico_pos (50 , 50);
+     Pico_Pos pos = pico_pos((Pico_Pct){50, 50});
      while (1) {
          pico_output_clear () ;
          pico_output_draw_image ( pos , " tux . png " );
